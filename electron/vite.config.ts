4078c282a42b23360dcaf962d99c956c4566b7df
Kouji Matsui <k@kekyo.net>
2023-12-25 11:53:23 +0900

    Modified `vite.config.ts`.
    Added `electron` and `renderer` vite plugin.

--- electron/vite.config.ts
+++ electron/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    electron({ entry: 'electron/main.ts' }),
+    renderer()],
 })

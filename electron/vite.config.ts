ca2fdc54a8efee84ad320c21ece0e6f53f8b016f
Kouji Matsui <k@kekyo.net>
2023-12-26 11:50:27 +0900

    Cleaned up dest directory when start for building.

--- electron/vite.config.ts
+++ electron/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
+import { rmSync } from 'node:fs'
 import react from '@vitejs/plugin-react-swc'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    electron({ entry: 'electron/main.ts' }),
-    renderer()],
-})
+export default defineConfig(() => {
+  rmSync("dist-electron", { recursive: true, force: true });
+  return {
+    plugins: [
+      react(),
+      electron({ entry: 'electron/main.ts' }),
+      renderer()
+    ]
+  };
+});
